22d74a8cf1e5c439aeae72bf84feff4849ad56d0
Alex Hovakimyan <hovak101@gmail.com>
2025-01-05 11:54:37 -0800

    updated interests

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,7 +8,7 @@ function Profile() {
             <img src={alexPhoto} className='profile-photo'/>
             <div className="bio-link-container">
                 <p className="profile-bio">I'm a computer science major at San Jose State University. 
-                I'm interested in <strong className="interests">robotics and computer vision</strong>. I've done a lot of 
+                My primary interests have been <strong className="interests">robotics, computer vision, ML, and web development</strong>, but the list is still growing. I've done a lot of 
                 personal and team projects, so please check them out below!</p>
                 <Links/>
             </div>
